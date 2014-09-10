d734ba01c6e5c2b484d2ef4cb6f6f48acf6f5442
LorincziIstvan <lorinczi_isti@yahoo.com>
2014-09-10 11:32:16 +0300

    new Jenkins

---
+++ src/test/java/edu/msg/chop2/test/TestSuit.java
@@ -0,0 +1,12 @@
+package edu.msg.chop2.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ ChopTest.class })
+public class TestSuit {
+
+}
